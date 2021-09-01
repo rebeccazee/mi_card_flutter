ec9e7ec71cf4e4c98db0fd51b441c0a51ad18fcd
Rebecca Zee <ryz283@gmail.com>
2021-09-01 13:02:33 -0400

    2. Hot Reload and Hot Restart - Flutter Power Tools (Ver1)

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('Demo'),
+        ),
+        body: Center(
+          child: Text('My name is Rebecca'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          backgroundColor: Colors.red,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
 }
