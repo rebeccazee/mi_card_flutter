d1e31b033947751f4821aceff2ddc42f46ace3d5
Rebecca Zee <ryz283@gmail.com>
2021-09-02 03:05:36 -0400

    6. Tapping into Widget Properties

--- lib/main.dart
+++ lib/main.dart
@@ -17,32 +17,20 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            // mainAxisSize: MainAxisSize.min,
-            // verticalDirection: VerticalDirection.down,
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
             children: [
-              Container(
-                width: 30,
-                color: Colors.white,
-                child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://scontent-lga3-1.xx.fbcdn.net/v/t31.18172-8/1015108_145354002325220_630585834_o.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=D7XesVKrZQYAX-XP8qq&tn=fY-_kJCT2s8aQ_JP&_nc_ht=scontent-lga3-1.xx&oh=461bdc682a648f2d6248ac388e4056f4&oe=6157792B'),
               ),
-              SizedBox(
-                width: 30,
-              ),
-              Container(
-                width: 30,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              SizedBox(
-                width: 30,
-              ),
-              Container(
-                width: 30,
-                color: Colors.red,
-                child: Text('Container 3'),
+              Text(
+                'Rebecca Zee',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ],
           ),
